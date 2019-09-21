62f562b16a1e92b23d6e33288be612979d766e87
Adriana Jauregui <adriana.jauregui@formiik.com>
2019-09-21 13:46:48 -0500

    first exercise

--- Sign.playground/Contents.swift
+++ Sign.playground/Contents.swift
@@ -2,4 +2,64 @@ import UIKit
 import GameplayKit
 
 
-var str = "Hello, playground"
+ enum Sign {
+    case piedra, papel, tijeras
+    
+    var descripcion: String {
+        switch self {
+        case .piedra:
+            return "👊"
+        case .tijeras:
+            return "✌️"
+        default:
+            return "🖐"
+        }
+    }
+    
+    func compare(singComputer: Sign) -> GameSet {
+        
+        var result = GameSet.inicio
+        let computerSign = singComputer
+        
+        if self == singComputer {
+            result = .empatar
+        }
+        else {
+            
+            if computerSign == .papel {
+                if self == .piedra {
+                    result = .perder
+                }
+                else {
+                    result = .ganar
+                }
+            }
+            
+            if computerSign == .piedra {
+                if self == .papel {
+                    result = .ganar
+                }
+                else {
+                    result = .perder
+                }
+            }
+            if computerSign == .tijeras{
+                if self == .papel {
+                    result = .perder
+                }
+                else {
+                    result = .ganar
+                }
+            }
+        }
+        
+        return result
+    }
+}
+
+enum  GameSet: String{
+    case inicio, ganar, perder, empatar
+    
+}
+
+print
